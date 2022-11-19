411ddde5d16be04008581ab665d3b6833f9b7d61
chaturvedi-anil <chaturvedia560@gmail.com>
2022-11-19 12:21:56 +0530

    added routes and controllers for user

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,9 @@ const router=express.Router();
 const homeController=require('../controllers/home_controller');
 
 console.log('router loaded');
+//for home page
 router.get('/', homeController.home);
+//for any request with /users routes, i will give control to /routes/users
+router.use('/users', require('../routes/users'));
+
 module.exports=router;
---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const { route } = require('.');
+const router=express.Router();
+const userController=require('../controllers/user_controller');
+
+router.get('/profile', userController.profile);
+
+module.exports=router;
