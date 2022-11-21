2b5447ef2781b8cf984f3ca1bc0fbe02c93e623d
chaturvedi-anil <chaturvedia560@gmail.com>
2022-11-21 10:27:36 +0530

    setup user profile page and showing details

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,9 +1,27 @@
 const User=require('../models/user');
 module.exports.profile=function(req, res)
 {
-    return res.render('users', {
-        title: "Users"
-    });
+    if(req.cookies.user_id)
+    {
+        User.findById(req.cookies.user_id, function(err, user)
+        {
+            if(user)
+            {
+                return res.render('users',{
+                    title: "User Profile",
+                    user: user
+                });
+            }
+            else
+            {
+                return res.redirect('/users/sign-in');
+            }
+        });
+    }
+    else
+    {
+        return res.redirect('/users/sign-in');
+    }
 }
 
 module.exports.signIn=function(req, res)
