91485b0383e0e90dd0d4e2a787376566542ae643
chaturvedi-anil <chaturvedia560@gmail.com>
2022-12-05 15:14:54 +0530

    showing post and populate the user and showing username in home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,22 @@
+const Post = require('../models/post');
 module.exports.home=function(req, res)
 {
-    return res.render('home', {
-        title:"Home"
+    // Post.find({}, function(err, posts)
+    // {
+    //     return res.render('home', {
+    //         title:"Codeial | Home",
+    //         posts: posts
+    //     });
+    // })
+
+    //polpulate the user of each post
+
+    Post.find({}).populate('user').exec(function(err, posts)
+    {
+        return res.render('home',{
+            title: "Codeial | Home",
+            posts:posts
+        });
     });
+    
 }
